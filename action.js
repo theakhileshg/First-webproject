5468f728aca0889eb804fdf97616e1e1d2133a83
AKHILESH <70057995+wiz-ak@users.noreply.github.com>
2021-05-17 20:26:45 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function pbt(){
+    document.body.style.background="purple"
+}
+
+function gbt(){
+    document.body.style.background="green"
+}
+
+function ybt(){
+    document.body.style.background="yellow"
+}
